92a9aa3a9d5469aa516499de907b78e8ce333ff3
ez7mz <mesrarhamza48@gmail.com>
2023-04-24 21:44:00 +0100

    Reconfig env

--- src/main/java/ma/smartcity/smartcity/security/config/WebSecurityConfig.java
+++ src/main/java/ma/smartcity/smartcity/security/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests()
+                .authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/api/v*/**", "/Assets/**", "/", "/about",  "/forgot-password", "/coming-soon")
                 .permitAll()
                 .anyRequest().authenticated().and().formLogin()
